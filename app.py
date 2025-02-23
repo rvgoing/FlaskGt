a07e692335677ab91f7e8369ab46b30e07b0027c
rvgoing <97498926+rvgoing@users.noreply.github.com>
2025-02-24 01:54:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,39 @@ def hello():
     """Just say hello."""
     click.echo('Hello, Human!')
 
+def fetch_weather_data():
+    api_key = '98d3e2789160b7e80590153f2451c490'
+    url = f'http://api.openweathermap.org/data/2.5/onecall/timemachine'
+    params = {
+        'lat': 25.0330,
+        'lon': 121.5654,
+        'dt': 'TIMESTAMP',
+        'appid': api_key,
+        'units': 'metric'
+    }
+    temperatures = []
+    humidities = []
+    for timestamp in [int((datetime.datetime.utcnow() - datetime.timedelta(hours=i)).timestamp()) for i in range(1, 25)]:
+        params['dt'] = timestamp
+        response = requests.get(url, params=params)
+        data = response.json()
+        temperatures.append(data['current']['temp'])
+        humidities.append(data['current']['humidity'])
+    return temperatures, humidities
+
 @app.route('/chart')
 def chart():
-    # Generate a simple chart
-    plt.figure(figsize=(5, 5))
-    plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-    plt.title('Sample Chart')
+    temperature, humidity = fetch_weather_data()
+    hours = list(range(24))
 
-    # Save the chart to a BytesIO object
+    plt.figure(figsize=(10, 5))
+    plt.plot(hours, temperature, label='Temperature (°C)')
+    plt.plot(hours, humidity, label='Humidity (%)')
+    plt.xlabel('Hours')
+    plt.ylabel('Values')
+    plt.title('Temperature and Humidity in Taipei City (Past 24 Hours)')
+    plt.legend()
+
     img = io.BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
